2ac42b70d97e96935262a438e63029ddfdadb145
D Lee <david.lee@softrams.com>
2024-12-09 11:20:41 -0600

    updates

--- git/install.sh
+++ git/install.sh
@@ -4,7 +4,7 @@ set -e
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-ln -nfs "$DIR/gitignore_global" ~/.gitignore_global
+ln -nfs "$DIR/.gitignore_global" ~/.gitignore_global
 
 cp "$DIR/.gitconfig" ~/.gitconfig
 ln -nfs "$DIR/.gitmessage" ~/.gitmessage
